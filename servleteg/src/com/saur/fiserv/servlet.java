890ab24d548249da0ea9b66b6136e50a944fd5db
hashirama971 <saurabh22397@gmail.com>
2021-07-29 13:02:33 +0530

    sevlet tag

---
+++ servleteg/src/com/saur/fiserv/servlet.java
@@ -0,0 +1,28 @@
+package com.saur.fiserv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class servlet extends HttpServlet {
+	
+
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+
+		PrintWriter out=res.getWriter();
+		out.println("<h1>hello world</h1>");
+		
+		System.out.println("in the do get method");	
+	}
+
+   public void doPost(HttpServletRequest req,HttpServletResponse res)
+   {
+	   System.out.println("in the post method");
+   }
+
+}
+
